bf7947bb10ad1d2fd3634c57099e0f8a05be30c8
AustEcon <AustEcon0922@gmail.com>
2020-07-11 14:25:34 +1200

    small tidy ups

--- electrumsv_sdk/runners.py
+++ electrumsv_sdk/runners.py
@@ -11,18 +11,6 @@ def run_electrumsv_node():
 
 
 def run_electrumx_server():
-    electrumx_env_vars = {
-        "DB_DIRECTORY": Config.depends_dir_electrumx_data.__str__(),
-        "DAEMON_URL": "http://rpcuser:rpcpassword@127.0.0.1:18332",
-        "DB_ENGINE": "leveldb",
-        "SERVICES": "tcp://:51001,rpc://",
-        "COIN": "BitcoinSV",
-        "COST_SOFT_LIMIT": "0",
-        "COST_HARD_LIMIT": "0",
-        "MAX_SEND": "10000000",
-        "LOG_LEVEL": "debug",
-        "NET": "regtest",
-    }
     if sys.platform == "win32":
         electrumx_server_script = Config.run_scripts_dir.joinpath("electrumx.bat")
     else:
