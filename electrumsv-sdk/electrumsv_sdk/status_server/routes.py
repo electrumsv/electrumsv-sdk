cfdea2d292dc631929e86aa6c3a99d843eb1e9fc
AustEcon <AustEcon0922@gmail.com>
2020-09-23 13:11:52 +1200

    status monitor should return an internal server error message to client

--- electrumsv-sdk/electrumsv_sdk/status_server/routes.py
+++ electrumsv-sdk/electrumsv_sdk/status_server/routes.py
@@ -9,19 +9,29 @@ logger = logging.getLogger("trinket-routes")
 
 # /api/status/get_status
 async def get_status(app, request):
-    component_state = []
-    with app.file_lock:
-        if COMPONENT_STATE_PATH.exists():
-            with open(COMPONENT_STATE_PATH, "r") as f:
-                component_state = json.loads(f.read())
-    return Response.json(json.dumps(component_state))
+    try:
+        component_state = []
+        with app.file_lock:
+            if COMPONENT_STATE_PATH.exists():
+                with open(COMPONENT_STATE_PATH, "r") as f:
+                    component_state = json.loads(f.read())
+        return Response.json(json.dumps(component_state))
+    except Exception as e:
+        logger.exception(e)
+        payload = {"status": None, "error": str(e)}
+        return Response.json(json.dumps(payload), status=500)
 
 
 # /api/status/update_status
 async def update_status(app, request: Request):
-    app.update_status(json.loads(request.body))
-    payload = {"status": "success", "error": None}
-    return Response.json(json.dumps(payload))
+    try:
+        app.update_status(json.loads(request.body))
+        payload = {"status": "success", "error": None}
+        return Response.json(json.dumps(payload))
+    except Exception as e:
+        logger.exception(e)
+        payload = {"status": None, "error": str(e)}
+        return Response.json(json.dumps(payload), status=500)
 
 
 # /api/status/unsubscribe
@@ -32,9 +42,12 @@ async def unsubscribe(app, request):
 
 # /api/status/subscribe
 async def subscribe(app, request, websocket):
-    """subscribe for status updates"""
-    host = request.socket._socket.getpeername()[0]
-    port = request.socket._socket.getpeername()[1]
-    logger.debug(f"Got websocket connection: host={host}, port={port}")
-    msg = await websocket.recv()
-    logger.debug(f"Websocket client closed connection")
+    try:
+        """subscribe for status updates"""
+        host = request.socket._socket.getpeername()[0]
+        port = request.socket._socket.getpeername()[1]
+        logger.debug(f"Got websocket connection: host={host}, port={port}")
+        msg = await websocket.recv()
+        logger.debug(f"Websocket client closed connection")
+    except Exception as e:
+        logger.exception(e)
