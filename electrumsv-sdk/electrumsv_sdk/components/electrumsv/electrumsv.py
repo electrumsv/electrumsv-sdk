ad464e309a345ef934c02fe4787e632cdc4ef10b
AustEcon <AustEcon0922@gmail.com>
2020-10-13 15:58:25 +1300

    skeleton of plugin system

---
+++ electrumsv-sdk/electrumsv_sdk/components/electrumsv/electrumsv.py
@@ -0,0 +1,18 @@
+def install(app_state):
+    pass
+
+
+def start(app_state):
+    pass
+
+
+def stop(app_state):
+    pass
+
+
+def reset(app_state):
+    pass
+
+
+def status_check(app_state):
+    pass
---
+++ electrumsv-sdk/electrumsv_sdk/components/status_monitor/status_monitor.py
@@ -0,0 +1,18 @@
+def install(app_state):
+    app_state.install_tools.install_status_monitor()
+
+
+def start(app_state):
+    pass
+
+
+def stop(app_state):
+    pass
+
+
+def reset(app_state):
+    pass
+
+
+def status_check(app_state):
+    pass
