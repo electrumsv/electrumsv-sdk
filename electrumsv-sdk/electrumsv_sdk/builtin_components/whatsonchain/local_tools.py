7b1aa5b37eaa76c17785d014fa3f178207de8b1d
AustEcon <AustEcon0922@gmail.com>
2020-11-19 02:43:10 +1300

    update whatsonchain plugin

---
+++ electrumsv-sdk/electrumsv_sdk/builtin_components/whatsonchain/local_tools.py
@@ -0,0 +1,59 @@
+import logging
+import os
+import subprocess
+import sys
+
+from electrumsv_node import electrumsv_node
+
+from electrumsv_sdk.constants import SHELL_SCRIPTS_DIR, REMOTE_REPOS_DIR
+from electrumsv_sdk.abstract_plugin import AbstractPlugin
+from electrumsv_sdk.config import ImmutableConfig
+from electrumsv_sdk.utils import checkout_branch
+
+
+class LocalTools:
+    """helper for operating on plugin-specific state (like source dir, port, datadir etc.)"""
+
+    def __init__(self, plugin: AbstractPlugin):
+        self.plugin = plugin
+        self.plugin_tools = self.plugin.plugin_tools
+        self.config: ImmutableConfig = plugin.config
+        self.logger = logging.getLogger(self.plugin.COMPONENT_NAME)
+
+    def fetch_whatsonchain(self, url="https://github.com/AustEcon/woc-explorer.git",
+                           branch=''):
+        if not self.plugin.src.exists():
+            os.makedirs(self.plugin.src, exist_ok=True)
+            os.chdir(REMOTE_REPOS_DIR)
+            subprocess.run(f"git clone {url}", shell=True, check=True)
+
+            os.chdir(self.plugin.src)
+            checkout_branch(branch)
+
+    def packages_whatsonchain(self):
+        os.chdir(self.plugin.src)
+        process = subprocess.Popen("npm install", shell=True)
+        process.wait()
+        process = subprocess.Popen("npm run-script build", shell=True)
+        process.wait()
+
+    def generate_run_script(self):
+        os.makedirs(SHELL_SCRIPTS_DIR, exist_ok=True)
+        os.chdir(SHELL_SCRIPTS_DIR)
+        line1 = f"cd {self.plugin.src}"
+        line2 = f"call npm start" if sys.platform == "win32" else f"npm start"
+        self.plugin_tools.make_shell_script_for_component(list_of_shell_commands=[line1, line2],
+            component_name=self.plugin.COMPONENT_NAME)
+
+    def check_node_for_woc(self):
+        if not electrumsv_node.is_running():
+            return False
+
+        result = electrumsv_node.call_any("getinfo")
+        block_height = result.json()['result']['blocks']
+        if block_height == 0:
+            self.logger.error(f"Block height=0. "
+                 f"The Whatsonchain explorer requires at least 1 block to be mined. Hint: try: "
+                 f"'electrumsv-sdk node generate 1'")
+            return False
+        return True
--- electrumsv-sdk/electrumsv_sdk/builtin_components/whatsonchain/whatsonchain.py
+++ electrumsv-sdk/electrumsv_sdk/builtin_components/whatsonchain/whatsonchain.py
@@ -2,66 +2,68 @@ import logging
 import sys
 from typing import Optional
 
-from electrumsv_sdk.components import ComponentOptions, Component
+from electrumsv_sdk.abstract_plugin import AbstractPlugin
+from electrumsv_sdk.config import ImmutableConfig
+from electrumsv_sdk.plugin_tools import PluginTools
+from electrumsv_sdk.components import Component
 from electrumsv_sdk.utils import get_directory_name, kill_process
 
-from .install import fetch_whatsonchain, generate_run_script, packages_whatsonchain
-from .start import check_node_for_woc
-
-DEFAULT_PORT = 3002
-RESERVED_PORTS = {DEFAULT_PORT}
-COMPONENT_NAME = get_directory_name(__file__)
-logger = logging.getLogger(COMPONENT_NAME)
+from .local_tools import LocalTools
 
 
-def install(app_state):
-    repo = app_state.global_cli_flags[ComponentOptions.REPO]
-    if not repo == "":  # default
-        logger.error("ignoring --repo flag for whatsonchain - not applicable.")
+class Plugin(AbstractPlugin):
+    DEFAULT_PORT = 3002
+    RESERVED_PORTS = {DEFAULT_PORT}
+    COMPONENT_NAME = get_directory_name(__file__)
+    logger = logging.getLogger(COMPONENT_NAME)
 
-    # 1) configure_paths (SEE BELOW)
-    app_state.src_dir = app_state.remote_repos_dir.joinpath("woc-explorer")
+    def __init__(self, config: ImmutableConfig):
+        self.config = config
+        self.plugin_tools = PluginTools(self, self.config)
+        self.tools = LocalTools(self)
+        self.logger = logging.getLogger(self.COMPONENT_NAME)
 
-    # 2) fetch (as needed) (SEE BELOW)
-    fetch_whatsonchain(app_state, url="https://github.com/AustEcon/woc-explorer.git", branch='')
+        self.src_dir = self.plugin_tools.get_source_dir("woc-explorer")
+        self.datadir = None  # N/A
+        self.id = self.plugin_tools.get_id(self.COMPONENT_NAME)
+        self.port = None  # N/A
+        self.component_info: Optional[Component] = None
 
-    # 3) pip install (or npm install) packages/dependencies (SEE BELOW)
-    packages_whatsonchain(app_state)
+    def install(self):
+        if not self.config.repo == "":  # default
+            self.logger.error("ignoring --repo flag for whatsonchain - not applicable.")
+        self.tools.fetch_whatsonchain(url="https://github.com/AustEcon/woc-explorer.git", branch='')
+        self.tools.packages_whatsonchain()
+        self.logger.debug(f"Installed whatsonchain explorer")
 
-    # 4) generate run script (SEE BELOW)
-    generate_run_script(app_state)
+    def start(self):
+        self.logger.debug(f"Starting whatsonchain explorer...")
+        self.tools.generate_run_script()
+        if not self.tools.check_node_for_woc():
+            sys.exit(1)
 
+        script_path = self.plugin_tools.derive_shell_script_path(self.COMPONENT_NAME)
+        process = self.plugin_tools.spawn_process(f"{script_path}")
+        id = self.plugin_tools.get_id(self.COMPONENT_NAME)
+        self.component_info = Component(id, process.pid, self.COMPONENT_NAME, str(self.src_dir),
+            "http://127.0.0.1:3002")
 
-def start(app_state):
-    logger.debug(f"Starting whatsonchain explorer...")
-    if not check_node_for_woc():
-        sys.exit(1)
+    def stop(self):
+        """some components require graceful shutdown via a REST API or RPC API but most can use the
+        generic 'app_state.kill_component()' function to track down the pid and kill the process."""
+        self.plugin_tools.call_for_component_id_or_type(self.COMPONENT_NAME, callable=kill_process)
+        self.logger.info(f"stopped selected {self.COMPONENT_NAME} instance(s) (if any)")
 
-    script_path = app_state.derive_shell_script_path(COMPONENT_NAME)
-    process = app_state.spawn_process(f"{script_path}")
-    id = app_state.get_id(COMPONENT_NAME)
-    app_state.component_info = Component(id, process.pid, COMPONENT_NAME,
-        str(app_state.src_dir), "http://127.0.0.1:3002")
+    def reset(self):
+        self.logger.info("resetting the whatsonchain is not applicable")
 
-
-def stop(app_state):
-    """some components require graceful shutdown via a REST API or RPC API but most can use the
-    generic 'app_state.kill_component()' function to track down the pid and kill the process."""
-    app_state.call_for_component_id_or_type(COMPONENT_NAME, callable=kill_process)
-    logger.info(f"stopped selected {COMPONENT_NAME} instance(s) (if any)")
-
-
-def reset(app_state):
-    logger.info("resetting the whatsonchain is not applicable")
-
-
-def status_check(app_state) -> Optional[bool]:
-    """
-    True -> ComponentState.RUNNING;
-    False -> ComponentState.FAILED;
-    None -> skip status monitoring updates (e.g. using app's cli interface transiently)
-    """
-    is_running = app_state.is_component_running_http(
-        status_endpoint=app_state.component_info.status_endpoint,
-        retries=4, duration=3, timeout=1.0, http_method='get')
-    return is_running
+    def status_check(self) -> Optional[bool]:
+        """
+        True -> ComponentState.RUNNING;
+        False -> ComponentState.FAILED;
+        None -> skip status monitoring updates (e.g. using app's cli interface transiently)
+        """
+        is_running = self.plugin_tools.is_component_running_http(
+            status_endpoint=self.component_info.status_endpoint,
+            retries=4, duration=3, timeout=1.0, http_method='get')
+        return is_running
