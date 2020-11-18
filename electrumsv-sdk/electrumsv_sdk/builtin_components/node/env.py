af0cf4e72fde578cc59e9e13b602271e32719211
AustEcon <AustEcon0922@gmail.com>
2020-11-19 02:36:44 +1300

    update node plugin

--- electrumsv-sdk/electrumsv_sdk/builtin_components/node/env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-NODE_PORT = os.environ.get("NODE_PORT")  # if None -> set by usual deterministic allocation
-NODE_RPCALLOWIP = os.environ.get("NODE_RPCALLOWIP")  # else 127.0.0.1
-NODE_RPCBIND = os.environ.get("NODE_RPCBIND")
