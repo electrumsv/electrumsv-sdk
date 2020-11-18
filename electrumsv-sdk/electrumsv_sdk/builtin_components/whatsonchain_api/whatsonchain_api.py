839fa6cd517c07e5c4839326f34ddf761aebe389
AustEcon <AustEcon0922@gmail.com>
2020-11-19 02:43:37 +1300

    update whatsonchain_api plugin

--- electrumsv-sdk/electrumsv_sdk/builtin_components/whatsonchain_api/whatsonchain_api.py
+++ electrumsv-sdk/electrumsv_sdk/builtin_components/whatsonchain_api/whatsonchain_api.py
@@ -1,50 +1,62 @@
 import logging
 import os
+import sys
 from pathlib import Path
 from typing import Optional
 
-from electrumsv_sdk.app_state import AppState
+from electrumsv_sdk.abstract_plugin import AbstractPlugin
+from electrumsv_sdk.config import ImmutableConfig
 from electrumsv_sdk.components import Component
 from electrumsv_sdk.utils import get_directory_name, kill_process
+from electrumsv_sdk.plugin_tools import PluginTools
 
-from .server_app import PING_URL, SERVER_PORT
-
-COMPONENT_NAME = get_directory_name(__file__)
-COMPONENT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
-SCRIPT_PATH = COMPONENT_PATH / "server_app.py"
-
-RESERVED_PORTS = {SERVER_PORT}
-
-logger = logging.getLogger(COMPONENT_NAME)
+from . import server_app
 
 
-def install(app_state: AppState) -> None:
-    pass
+class Plugin(AbstractPlugin):
+    SERVER_HOST = server_app.SERVER_HOST
+    SERVER_PORT = server_app.SERVER_PORT
+    RESERVED_PORTS = {SERVER_PORT}
+    PING_URL = server_app.PING_URL
 
+    COMPONENT_NAME = get_directory_name(__file__)
+    COMPONENT_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
+    SCRIPT_PATH = COMPONENT_PATH / "server_app.py"
 
-def start(app_state: AppState) -> None:
-    id = app_state.get_id(COMPONENT_NAME)
-    process = app_state.spawn_process(f"{app_state.python} {SCRIPT_PATH}")
-    app_state.component_info = Component(id, process.pid, COMPONENT_NAME,
-        COMPONENT_PATH, PING_URL)
+    def __init__(self, config: ImmutableConfig):
+        self.config = config
+        self.plugin_tools = PluginTools(self, self.config)
+        self.logger = logging.getLogger(self.COMPONENT_NAME)
 
+        self.src = self.COMPONENT_PATH
+        self.datadir = None  # dynamically allocated
+        self.id = None  # dynamically allocated
+        self.port = None  # dynamically allocated
+        self.component_info: Optional[Component] = None
 
-def stop(app_state: AppState) -> None:
-    logger.debug("Attempting to kill the process if it is even running")
-    app_state.call_for_component_id_or_type(COMPONENT_NAME, callable=kill_process)
+    def install(self) -> None:
+        self.logger.debug(f"Installing {self.COMPONENT_NAME} is not applicable")
 
+    def start(self) -> None:
+        id = self.plugin_tools.get_id(self.COMPONENT_NAME)
+        process = self.plugin_tools.spawn_process(f"{sys.executable} {self.SCRIPT_PATH}")
+        self.component_info = Component(id, process.pid, self.COMPONENT_NAME,
+            self.COMPONENT_PATH, self.PING_URL)
 
-def reset(app_state: AppState) -> None:
-    pass
+    def stop(self) -> None:
+        self.logger.debug("Attempting to kill the process if it is even running")
+        self.plugin_tools.call_for_component_id_or_type(self.COMPONENT_NAME, callable=kill_process)
 
+    def reset(self) -> None:
+        pass
 
-def status_check(app_state: AppState) -> Optional[bool]:
-    """
-    True -> ComponentState.RUNNING;
-    False -> ComponentState.FAILED;
-    None -> skip status monitoring updates (e.g. using app's cli interface transiently)
-    """
-    is_running = app_state.is_component_running_http(
-        status_endpoint=app_state.component_info.status_endpoint,
-        retries=5, duration=2, timeout=1.0)
-    return is_running
+    def status_check(self) -> Optional[bool]:
+        """
+        True -> ComponentState.RUNNING;
+        False -> ComponentState.FAILED;
+        None -> skip status monitoring updates (e.g. using app's cli interface transiently)
+        """
+        is_running = self.plugin_tools.is_component_running_http(
+            status_endpoint=self.component_info.status_endpoint,
+            retries=5, duration=2, timeout=1.0)
+        return is_running
