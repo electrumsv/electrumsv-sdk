a8c3c05914424f2fd07600236f4ff975bb7baa74
AustEcon <AustEcon0922@gmail.com>
2020-11-19 02:42:03 +1300

    update merchant_api plugin

--- electrumsv-sdk/electrumsv_sdk/builtin_components/merchant_api/merchant_api.py
+++ electrumsv-sdk/electrumsv_sdk/builtin_components/merchant_api/merchant_api.py
@@ -1,61 +1,76 @@
 import logging
 import os
+from pathlib import Path
 from typing import Optional
 
+from electrumsv_node import electrumsv_node
+
+from electrumsv_sdk.abstract_plugin import AbstractPlugin
+from electrumsv_sdk.config import ImmutableConfig
 from electrumsv_sdk.components import Component
 from electrumsv_sdk.utils import get_directory_name, kill_process
+from electrumsv_sdk.plugin_tools import PluginTools
 
 from .install import download_and_install, create_settings_file, get_run_path
 
-DEFAULT_PORT = 45111
-RESERVED_PORTS = {DEFAULT_PORT}
-COMPONENT_NAME = get_directory_name(__file__)
-logger = logging.getLogger(COMPONENT_NAME)
 
-NODE_RPC_PORT = 18332
-NODE_RPC_USERNAME = "rpcuser"
-NODE_RPC_PASSWORD = "rpcpassword"
-NODE_ZMQ_PORT = 28332
+class Plugin(AbstractPlugin):
 
+    NODE_RPC_PORT = 18332
+    NODE_RPC_USERNAME = "rpcuser"
+    NODE_RPC_PASSWORD = "rpcpassword"
+    NODE_ZMQ_PORT = 28332
 
-def install(app_state):
-    app_state.component_datadir, app_state.component_id = \
-        app_state.get_component_datadir(COMPONENT_NAME)
-    download_and_install(app_state.component_datadir)
-    create_settings_file(app_state.component_datadir, DEFAULT_PORT, NODE_RPC_PORT,
-        NODE_RPC_USERNAME, NODE_RPC_PASSWORD, NODE_ZMQ_PORT)
+    DEFAULT_PORT = 45111
+    RESERVED_PORTS = {DEFAULT_PORT}
+    COMPONENT_NAME = get_directory_name(__file__)
 
+    def __init__(self, config: ImmutableConfig):
+        self.config = config
+        self.plugin_tools = PluginTools(self, self.config)
+        self.logger = logging.getLogger(self.COMPONENT_NAME)
 
-def start(app_state):
-    logger.debug(f"Starting Merchant API")
-    # The primary reason we need this to be the current directory is so that the `settings.conf`
-    # file is directly accessible to the MAPI executable (it should look there first).
-    os.chdir(app_state.component_datadir)
-    # Get the path to the executable file.
-    run_path = get_run_path(app_state.component_datadir)
-    process = app_state.spawn_process(str(run_path))
-    app_state.component_info = Component(app_state.component_id, process.pid, COMPONENT_NAME,
-        app_state.component_datadir, "???")
+        self.src = self.plugin_tools.get_source_dir(dirname="merchant_api")
+        self.datadir = None  # dynamically allocated
+        self.id = None  # dynamically allocated
+        self.port = None  # dynamically allocated
+        self.component_info: Optional[Component] = None
 
+    def install(self):
+        download_and_install(self.src)
+        create_settings_file(self.src, self.DEFAULT_PORT, self.NODE_RPC_PORT,
+            self.NODE_RPC_USERNAME, self.NODE_RPC_PASSWORD, self.NODE_ZMQ_PORT)
+        self.logger.debug(f"Installed Merchant API")
 
-def stop(app_state):
-    """some components require graceful shutdown via a REST API or RPC API but most can use the
-    generic 'app_state.kill_component()' function to track down the pid and kill the process."""
-    app_state.call_for_component_id_or_type(COMPONENT_NAME, callable=kill_process)
-    logger.info(f"stopped selected {COMPONENT_NAME} instance(s) (if any)")
+    def start(self):
+        self.logger.debug(f"Starting Merchant API")
+        self.id = self.plugin_tools.get_id(self.COMPONENT_NAME)
+        self.port = self.plugin_tools.allocate_port()
+        # The primary reason we need this to be the current directory is so that the `settings.conf`
+        # file is directly accessible to the MAPI executable (it should look there first).
+        os.chdir(self.src)
+        # Get the path to the executable file.
+        run_path = get_run_path(self.src)
+        process = self.plugin_tools.spawn_process(str(run_path))
+        self.component_info = Component(self.id, process.pid, self.COMPONENT_NAME,
+            self.src, "???")
 
+    def stop(self):
+        """some components require graceful shutdown via a REST API or RPC API but most can use the
+        generic 'app_state.kill_component()' function to track down the pid and kill the process."""
+        self.plugin_tools.call_for_component_id_or_type(self.COMPONENT_NAME, callable=kill_process)
+        self.logger.info(f"stopped selected {self.COMPONENT_NAME} instance(s) (if any)")
 
-def reset(app_state):
-    logger.info("resetting Merchant API is not applicable")
+    def reset(self):
+        self.logger.info("resetting Merchant API is not applicable")
 
-
-def status_check(app_state) -> Optional[bool]:
-    """
-    True -> ComponentState.RUNNING;
-    False -> ComponentState.FAILED;
-    None -> skip status monitoring updates (e.g. using app's cli interface transiently)
-    """
-    # is_running = app_state.is_component_running_http(
-    #     status_endpoint=app_state.component_info.status_endpoint,
-    #     retries=4, duration=3, timeout=1.0, http_method='get')
-    return True
+    def status_check(self) -> Optional[bool]:
+        """
+        True -> ComponentState.RUNNING;
+        False -> ComponentState.FAILED;
+        None -> skip status monitoring updates (e.g. using app's cli interface transiently)
+        """
+        # is_running = app_state.is_component_running_http(
+        #     status_endpoint=app_state.component_info.status_endpoint,
+        #     retries=4, duration=3, timeout=1.0, http_method='get')
+        return True
