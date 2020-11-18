e76290544ed3610ffa0711cce2f1c0a2c966dfd4
AustEcon <AustEcon0922@gmail.com>
2020-11-19 03:05:23 +1300

    update pipelines and fix small bug (AppState needs to be instantiated first to create all necessary directories on first startup)

--- electrumsv-sdk/electrumsv_sdk/__main__.py
+++ electrumsv-sdk/electrumsv_sdk/__main__.py
@@ -11,6 +11,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(name)-24s %(message)s'
 logger = logging.getLogger("main")
 logger_requests = logging.getLogger("urllib3").setLevel(logging.WARNING)
 
+
 def main():
     """
     Command-line interface for the ElectrumSV Software Development Kit
@@ -23,32 +24,32 @@ def main():
     b) the ability to string multiple subcommands + optional args together into a single cli
     command.
     """
-    argparser = ArgParser()
-    config: ImmutableConfig = argparser.manual_argparsing(sys.argv)
-    argparser.validate_cli_args()
-
-    app_state = AppState(config)
+    app_state = AppState()
     app_state.handle_first_ever_run()
 
     # Call relevant entrypoint
-    if config.namespace == NameSpace.INSTALL:
-        app_state.controller.install(config)  # start() -> status_check() entrypoint of plugin
+    if app_state.config.namespace == NameSpace.INSTALL:
+        # -> install() entrypoint of plugin
+        app_state.controller.install(app_state.config)
 
-    if config.namespace == NameSpace.START:
-        app_state.controller.start(config)  # start() -> status_check() entrypoint of plugin
+    if app_state.config.namespace == NameSpace.START:
+        # -> start() -> status_check() entrypoint of plugin
+        app_state.controller.start(app_state.config)
 
-    if config.namespace == NameSpace.STOP:
-        app_state.controller.stop(config)  # -> stop() entrypoint of plugin
+    if app_state.config.namespace == NameSpace.STOP:
+        # -> stop() entrypoint of plugin
+        app_state.controller.stop(app_state.config)
 
-    if config.namespace == NameSpace.RESET:
-        app_state.controller.reset(config)  # -> reset() entrypoint of plugin
+    if app_state.config.namespace == NameSpace.RESET:
+        # -> reset() entrypoint of plugin
+        app_state.controller.reset(app_state.config)
 
     # Special built-in execution pathway (not part of plugin system)
-    if config.namespace == NameSpace.NODE:
-        app_state.controller.node(config)
+    if app_state.config.namespace == NameSpace.NODE:
+        app_state.controller.node(app_state.config)
 
     # Http 'GET' request to status_monitor (which itself is a plugin component of the SDK)
-    if config.namespace == NameSpace.STATUS:
+    if app_state.config.namespace == NameSpace.STATUS:
         app_state.controller.status()
 
 
