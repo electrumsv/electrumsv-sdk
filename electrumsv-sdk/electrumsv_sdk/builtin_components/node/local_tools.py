a176be6765d322d3e9d0ae2622448f572b316c35
AustEcon <AustEcon0922@gmail.com>
2020-11-19 02:36:44 +1300

    update node plugin

---
+++ electrumsv-sdk/electrumsv_sdk/builtin_components/node/local_tools.py
@@ -0,0 +1,19 @@
+import logging
+import subprocess
+import sys
+
+from electrumsv_sdk.abstract_plugin import AbstractPlugin
+from electrumsv_sdk.config import ImmutableConfig
+
+
+class LocalTools:
+    """helper for operating on plugin-specific state (like source dir, port, datadir etc.)"""
+
+    def __init__(self, plugin: AbstractPlugin):
+        self.plugin = plugin
+        self.plugin_tools = self.plugin.plugin_tools
+        self.config: ImmutableConfig = plugin.config
+        self.logger = logging.getLogger(self.plugin.COMPONENT_NAME)
+
+    def fetch_node(self):
+        subprocess.run(f"{sys.executable} -m pip install electrumsv-node", shell=True, check=True)
--- electrumsv-sdk/electrumsv_sdk/builtin_components/node/node.py
+++ electrumsv-sdk/electrumsv_sdk/builtin_components/node/node.py
@@ -3,101 +3,105 @@ import os
 from pathlib import Path
 from typing import Optional, Dict
 
+from electrumsv_sdk.abstract_plugin import AbstractPlugin
+from electrumsv_sdk.config import ImmutableConfig
+from electrumsv_sdk.components import Component
+from electrumsv_sdk.utils import get_directory_name
+from electrumsv_sdk.plugin_tools import PluginTools
+
 from electrumsv_node import electrumsv_node
 
-from electrumsv_sdk.components import ComponentOptions, Component
-from electrumsv_sdk.utils import get_directory_name
-
-from .install import fetch_node, configure_paths
-from . import env
-
-DEFAULT_PORT = 18332
-DEFAULT_P2P_PORT_NODE = 18444
-RESERVED_PORTS = {DEFAULT_PORT, DEFAULT_P2P_PORT_NODE}
-COMPONENT_NAME = get_directory_name(__file__)
-logger = logging.getLogger(COMPONENT_NAME)
+from .local_tools import LocalTools
 
 
-def install(app_state):
-    """The node component has a pip installer at https://pypi.org/project/electrumsv-node/ and
-    only official releases from pypi are supported"""
-    repo = app_state.global_cli_flags[ComponentOptions.REPO]
-    if not repo == "":  # default
-        logger.error("ignoring --repo flag for node - not applicable.")
+class Plugin(AbstractPlugin):
 
-    configure_paths(app_state)
-    # 2) fetch (as needed) - (SEE BELOW)
-    fetch_node(app_state)
-    # 3) pip install (or npm install) packages/dependencies - (NOT APPLICABLE)
-    # 4) generate run script - (NOT APPLICABLE)
+    NODE_PORT = os.environ.get("NODE_PORT")  # if None -> set by usual deterministic allocation
+    NODE_RPCALLOWIP = os.environ.get("NODE_RPCALLOWIP")  # else 127.0.0.1
+    NODE_RPCBIND = os.environ.get("NODE_RPCBIND")
 
+    DEFAULT_PORT = 18332
+    DEFAULT_P2P_PORT = 18444
+    RESERVED_PORTS = {DEFAULT_PORT, DEFAULT_P2P_PORT}
+    COMPONENT_NAME = get_directory_name(__file__)
 
-def start(app_state):
-    extra_params = []
-    if env.NODE_RPCALLOWIP:
-        extra_params.append(f"-rpcallowip={env.NODE_RPCALLOWIP}")
-    if env.NODE_RPCBIND:
-        extra_params.append(f"-rpcbind={env.NODE_RPCBIND}")
-    if not extra_params:
-        extra_params = None
+    def __init__(self, config: ImmutableConfig):
+        self.config = config
+        self.plugin_tools = PluginTools(self, self.config)
+        self.tools = LocalTools(self)
+        self.logger = logging.getLogger(self.COMPONENT_NAME)
 
-    if env.NODE_PORT:
-        app_state.component_port = env.NODE_PORT
+        self.src = Path(electrumsv_node.FILE_PATH).parent
+        self.datadir = None  # dynamically allocated
+        self.id = None  # dynamically allocated
+        self.port = None  # dynamically allocated
+        self.p2p_port = None  # dynamically allocated
+        self.component_info: Optional[Component] = None
 
-    rpcport = app_state.component_port
-    p2p_port = app_state.component_p2p_port
-    data_path = app_state.component_datadir
-    component_id = app_state.component_id
+    def install(self):
+        """The node component has a pip installer at https://pypi.org/project/electrumsv-node/ and
+        only official releases from pypi are supported"""
+        self.tools.fetch_node()
 
-    process_pid = electrumsv_node.start(data_path=data_path, rpcport=rpcport,
-        p2p_port=p2p_port, network='regtest', extra_params=extra_params)
-    logging_path = Path(app_state.component_datadir).joinpath("regtest/bitcoind.log")
+    def start(self):
+        if self.NODE_PORT:
+            self.port = self.NODE_PORT
+        else:
+            self.port = self.plugin_tools.allocate_port()
+        self.datadir, self.id = self.plugin_tools.allocate_datadir_and_id()
+        self.p2p_port = self.plugin_tools.get_component_port(self.DEFAULT_P2P_PORT,
+            self.COMPONENT_NAME, self.id)
 
-    app_state.component_info = Component(component_id, process_pid, COMPONENT_NAME,
-        str(app_state.component_source_dir),
-        f"http://rpcuser:rpcpassword@127.0.0.1:{rpcport}",
-        logging_path=logging_path,
-        metadata={"datadir": str(app_state.component_datadir),
-                  "rpcport": rpcport,
-                  "p2p_port": p2p_port}
-    )
-    app_state.node_status_check_result = True
+        # env vars take precedence for port and dbdir
 
+        extra_params = []
+        if self.NODE_RPCALLOWIP:
+            extra_params.append(f"-rpcallowip={self.NODE_RPCALLOWIP}")
+        if self.NODE_RPCBIND:
+            extra_params.append(f"-rpcbind={self.NODE_RPCBIND}")
+        if not extra_params:
+            extra_params = None
 
-def stop(app_state):
-    """The bitcoin node requires graceful shutdown via the RPC API - a good example of why this
-    entrypoint is provided for user customizations (rather than always killing the process)."""
-    def stop_node(component_dict: Dict):
-        rpcport = component_dict.get("metadata").get("rpcport")
-        if not rpcport:
-            raise Exception("rpcport data not found")
-        electrumsv_node.stop(rpcport=rpcport)
-        logger.info(f"terminated: {component_dict.get('id')}")
+        process_pid = electrumsv_node.start(data_path=str(self.datadir), rpcport=self.port,
+            p2p_port=self.p2p_port, network='regtest', extra_params=extra_params)
+        logging_path = Path(self.datadir).joinpath("regtest/bitcoind.log")
 
-    app_state.call_for_component_id_or_type(COMPONENT_NAME, callable=stop_node)
-    logger.info(f"stopped selected {COMPONENT_NAME} instance(s) (if any)")
+        self.component_info = Component(self.id, process_pid, self.COMPONENT_NAME,
+            str(self.datadir), f"http://rpcuser:rpcpassword@127.0.0.1:{self.port}",
+            logging_path=logging_path,
+            metadata={"DATADIR": str(self.datadir), "rpcport": self.port, "p2p_port": self.p2p_port}
+        )
+        self.node_status_check_result = True
 
+    def stop(self):
+        """The bitcoin node requires graceful shutdown via the RPC API - a good example of why this
+        entrypoint is provided for user customizations (rather than always killing the process)."""
+        def stop_node(component_dict: Dict):
+            rpcport = component_dict.get("metadata").get("rpcport")
+            if not rpcport:
+                raise Exception("rpcport data not found")
+            electrumsv_node.stop(rpcport=rpcport)
+            self.logger.info(f"terminated: {component_dict.get('id')}")
 
-def reset(app_state):
-    # Todo for this + stop() can likely generalise it by passing it a 'callable' (function that
-    #  takes one argument (component_name)... and the helper function can do the rest... calling
-    #  our custom reset or stop function as appropriate for --id or component_name settings.
-    def reset_node(component_dict: Dict):
-        rpcport = component_dict.get("metadata").get("rpcport")
-        datadir = component_dict.get("metadata").get("datadir")
-        if not rpcport:
-            raise Exception("rpcport data not found")
-        electrumsv_node.reset(data_path=datadir, rpcport=rpcport)
-        logger.info(f"terminated: {component_dict.get('id')}")
+        self.plugin_tools.call_for_component_id_or_type(self.COMPONENT_NAME, callable=stop_node)
+        self.logger.info(f"stopped selected {self.COMPONENT_NAME} instance(s) (if any)")
 
-    app_state.call_for_component_id_or_type(COMPONENT_NAME, callable=reset_node)
-    logger.debug("Reset of RegTest bitcoin daemon completed successfully.")
+    def reset(self):
+        def reset_node(component_dict: Dict):
+            rpcport = component_dict.get("metadata").get("rpcport")
+            datadir = component_dict.get("metadata").get("DATADIR")
+            if not rpcport:
+                raise Exception("rpcport data not found")
+            electrumsv_node.reset(data_path=datadir, rpcport=rpcport)
+            self.logger.info(f"terminated: {component_dict.get('id')}")
 
+        self.plugin_tools.call_for_component_id_or_type(self.COMPONENT_NAME, callable=reset_node)
+        self.logger.debug("Reset of RegTest bitcoin daemon completed successfully.")
 
-def status_check(app_state) -> Optional[bool]:
-    """
-    True -> ComponentState.RUNNING;
-    False -> ComponentState.FAILED;
-    None -> skip status monitoring updates (e.g. using app's cli interface transiently)
-    """
-    return app_state.node_status_check_result
+    def status_check(self) -> Optional[bool]:
+        """
+        True -> ComponentState.RUNNING;
+        False -> ComponentState.FAILED;
+        None -> skip status monitoring updates (e.g. using app's cli interface transiently)
+        """
+        return self.node_status_check_result
