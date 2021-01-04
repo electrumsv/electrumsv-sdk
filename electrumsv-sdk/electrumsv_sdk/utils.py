d2ad52b422f13a4a77ec5aced3d5bc0214905625
AustEcon <AustEcon0922@gmail.com>
2021-01-04 15:36:35 +1300

    add colorama so that stdout on windows correctly converts ANSI colour codes in terminal output (for --inline and --new-terminal modes)
    - particularly noticeable with docker and ASP.NET applications

--- electrumsv-sdk/electrumsv_sdk/utils.py
+++ electrumsv-sdk/electrumsv_sdk/utils.py
@@ -9,6 +9,7 @@ import threading
 from pathlib import Path
 from typing import List, Dict, Optional
 
+import colorama
 import psutil
 import tailer
 from electrumsv_node import electrumsv_node
@@ -169,6 +170,11 @@ def get_sdk_datadir():
 
 def tail(logfile):
     for line in tailer.follow(open(logfile), delay=0.3):
+        "https://www.devdungeon.com/content/colorize-terminal-output-python"
+        # If using Windows, init() will cause anything sent to stdout or stderr
+        # will have ANSI color codes converted to the Windows versions. Hooray!
+        # If you are already using an ANSI compliant shell, it won't do anything
+        colorama.init()
         print(line)
 
 
