90a1f07698f876d424575fbd70ff1c7b85eb7a23
AustEcon <austecon0922@gmail.com>
2021-08-17 14:09:11 +1200

    Use a template for standardized_tests

--- electrumsv_sdk/app_state.py
+++ electrumsv_sdk/app_state.py
@@ -33,12 +33,6 @@ class AppState:
         self.config = Config(cli_inputs=self.cli_inputs)
         if self.cli_inputs.namespace == NameSpace.CONFIG:
             self.config.print_json()
-        logger.debug(f"\nConfiguration: \n"
-            f"\tSDK_HOME_DIR={self.config.SDK_HOME_DIR}\n"
-            f"\tREMOTE_REPOS_DIR={self.config.REMOTE_REPOS_DIR}\n"
-            f"\tDATADIR={self.config.DATADIR}\n"
-            f"\tLOGS_DIR={self.config.LOGS_DIR}\n"
-            f"\tPYTHON_LIB_DIR={self.config.PYTHON_LIB_DIR}\n")
 
         self.argparser.validate_cli_args()
 
