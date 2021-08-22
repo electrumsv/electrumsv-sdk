2f06e2d1e616913e6043aef1a7b93f99fb28dd66
AustEcon <austecon0922@gmail.com>
2021-08-22 17:04:30 +1200

    Ensure that .env file for mAPI is included in site-packages
    - Ensure that the check for all components starting does not have false negatives due to some components not ever registering in component_state.json

--- contrib/check_all_started.py
+++ contrib/check_all_started.py
@@ -1,8 +1,12 @@
 import logging
+import sys
+
 from electrumsv_sdk.components import ComponentStore
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger('script')
+
+NUM_COMPONENTS_STARTED = int(sys.argv[1])
 
 
 class ComponentFailedToStartError(Exception):
@@ -16,5 +20,9 @@ for component_name in component_state:
     if component_state[component_name]['component_state'] != 'Running':
         raise ComponentFailedToStartError(f"All components did not start sucessfully: "
             f"{component_state}")
+
+    if len(component_state) != NUM_COMPONENTS_STARTED:
+        raise ComponentFailedToStartError(f"Expected {NUM_COMPONENTS_STARTED} but only got: "
+            f"{len(component_state)}")
 
 logger.debug(f"Successfully started all components")
