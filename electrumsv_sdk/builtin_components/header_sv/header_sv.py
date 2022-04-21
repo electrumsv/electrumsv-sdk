bae8e7008fafecc486d941631f1170284f3404cf
AustEcon <AustEcon0922@gmail.com>
2022-04-22 07:57:36 +1200

    Add HeaderSV plugin

---
+++ electrumsv_sdk/builtin_components/header_sv/header_sv.py
@@ -0,0 +1,97 @@
+import logging
+import os
+import shutil
+import sys
+from pathlib import Path
+from typing import Optional, Set
+
+from electrumsv_sdk.builtin_components._common.utils import download_and_init_postgres
+from electrumsv_sdk.sdk_types import AbstractPlugin
+from electrumsv_sdk.config import CLIInputs, Config
+from electrumsv_sdk.components import Component, ComponentTypedDict
+from electrumsv_sdk.utils import get_directory_name, kill_process
+from electrumsv_sdk.plugin_tools import PluginTools
+
+from .install import download_and_install, get_run_command, HEADER_SV_VERSION, \
+    load_env_vars
+
+
+
+class Plugin(AbstractPlugin):
+
+    DEFAULT_PORT = 33444
+    RESERVED_PORTS: Set[int] = {DEFAULT_PORT}
+    COMPONENT_NAME = get_directory_name(__file__)
+
+    def __init__(self, cli_inputs: CLIInputs):
+        self.cli_inputs = cli_inputs
+        self.config = Config()
+        self.plugin_tools = PluginTools(self, self.cli_inputs)
+        self.logger = logging.getLogger(self.COMPONENT_NAME)
+
+        self.src = self.plugin_tools.get_source_dir(dirname="header_sv")
+        self.datadir: Optional[Path] = None  # dynamically allocated
+        self.id: Optional[str] = None  # dynamically allocated
+        self.port: Optional[int] = None  # dynamically allocated
+        self.component_info: Optional[Component] = None
+        download_and_init_postgres()  # only if necessary
+
+    def install(self) -> None:
+        assert self.src is not None  # typing bug in mypy
+        download_and_install(self.src)
+
+        self.logger.debug(f"Installed {self.COMPONENT_NAME}")
+
+    def start(self) -> None:
+        assert self.src is not None  # typing bug
+
+        self.logger.debug(f"Starting Header SV")
+        if not self.src.exists():
+            self.logger.error(f"source code directory does not exist - try 'electrumsv-sdk install "
+                              f"{self.COMPONENT_NAME}' to install the plugin first")
+            sys.exit(1)
+
+        self.id = self.plugin_tools.get_id(self.COMPONENT_NAME)
+        self.port = self.DEFAULT_PORT
+        # The primary reason we need this to be the current directory is so that the `settings.conf`
+        # file is directly accessible to the MAPI executable (it should look there first).
+        os.chdir(self.src)
+
+        # EXE RUN MODE
+        load_env_vars()
+        try:
+            command = get_run_command(self.src)
+        except FileNotFoundError:
+            self.logger.error(f"Could not find version: {HEADER_SV_VERSION} of "
+                f"HeaderSV. Have you tried re-running 'electrumsv-sdk install "
+                f"{self.COMPONENT_NAME}' to pull the latest version?")
+            return
+
+        logfile = self.plugin_tools.get_logfile_path(self.id)
+        status_endpoint = "http://localhost:33444/api/v1/chain/tips"
+
+        self.plugin_tools.spawn_process(str(command), env_vars=os.environ.copy(), id=self.id,
+            component_name=self.COMPONENT_NAME, src=self.src, logfile=logfile,
+            status_endpoint=status_endpoint)
+
+    def stop(self) -> None:
+        self.plugin_tools.call_for_component_id_or_type(self.COMPONENT_NAME, callable=kill_process)
+        self.logger.info(f"stopped selected {self.COMPONENT_NAME} instance (if running)")
+
+    def reset(self) -> None:
+
+        def reset_headersv(component_dict: ComponentTypedDict) -> None:
+            self.logger.debug("Resetting state of HeaderSV server...")
+            if sys.platform == 'win32':
+                datadir = Path(os.environ['LOCALAPPDATA']) / "Temp" / "jcl"
+            else:
+                datadir = Path("/tmp/jcl")
+
+            if datadir.exists():
+                shutil.rmtree(datadir)
+                os.mkdir(datadir)
+
+        self.plugin_tools.call_for_component_id_or_type(
+            self.COMPONENT_NAME, callable=reset_headersv)
+        self.logger.info("Reset of HeaderSV completed successfully")
+
---
+++ electrumsv_sdk/builtin_components/header_sv/install.py
@@ -0,0 +1,167 @@
+import logging
+import os
+import pathlib
+import platform
+
+import requests
+import sys
+import zipfile
+from typing import Dict, Any
+from urllib.parse import urlparse
+
+from electrumsv_sdk.config import Config
+from electrumsv_sdk.utils import get_directory_name
+
+
+VERSION = "0.0.1"  # standalone-build-release
+HEADER_SV_VERSION = "1.0.2"
+MODULE_DIR = pathlib.Path(os.path.dirname(os.path.abspath(__file__)))
+
+COMPONENT_NAME = get_directory_name(__file__)
+logger = logging.getLogger(COMPONENT_NAME)
+
+
+
+# The uri is copied from the Github repository release assets list.
+PREBUILT_ENTRIES = {
+    "Darwin": {
+        "uri": f"https://github.com/electrumsv/header-sv-binaries/releases/"
+               f"download/{VERSION}/headerSV-boot-{HEADER_SV_VERSION}.zip",
+        "exe": "headerSV-boot-1.0.2/lib/headerSV-1.0.2.jar",
+        "dirname": "MacOSXMerchantAPI",
+        "jre_uri": "https://github.com/adoptium/temurin17-binaries/releases/"
+                   "download/jdk-17.0.2%2B8/OpenJDK17U-jre_x64_mac_hotspot_17.0.2_8.tar.gz",
+        "jre_dirname": "jdk-17.0.2+8-jre"
+    },
+    "Linux": {
+        "uri": f"https://github.com/electrumsv/header-sv-binaries/releases/"
+               f"download/{VERSION}/headerSV-boot-{HEADER_SV_VERSION}.zip",
+        "exe": "headerSV-boot-1.0.2/lib/headerSV-1.0.2.jar",
+        "dirname": "LinuxMerchantAPI",
+        "jre_uri": "https://github.com/adoptium/temurin17-binaries/releases/"
+                   "download/jdk-17.0.3%2B7/OpenJDK17U-jre_x64_linux_hotspot_17.0.3_7.tar.gz",
+        "jre_dirname": "jdk-17.0.3+7-jre"
+    },
+    "Windows": {
+        "uri": f"https://github.com/electrumsv/header-sv-binaries/releases/download/{VERSION}/"
+               f"headerSV-boot-{HEADER_SV_VERSION}.zip",
+        "exe": "headerSV-boot-1.0.2/lib/headerSV-1.0.2.jar",
+        "dirname": "WindowsMerchantAPI",
+        "jre_uri": "https://github.com/adoptium/temurin17-binaries/releases/"
+                   "download/jdk-17.0.2%2B8/OpenJDK17U-jre_x64_windows_hotspot_17.0.2_8.zip",
+        "jre_dirname": "jdk-17.0.2+8-jre"
+    }
+}
+
+
+def _get_entry() -> Dict[Any, str]:
+    system_name = platform.system()
+    is_64bit = sys.maxsize > 2**32
+    if not is_64bit:
+        logger.error("32 bit OS is not supported - use a 64 bit version of python")
+        sys.exit()
+    entry_key = system_name
+    return PREBUILT_ENTRIES[entry_key]
+
+
+def download_and_install_jar(install_path: pathlib.Path) -> None:
+    """
+    Identify the correct pre-built release for the running Python version and:
+    - Download it if it is not already downloaded.
+    - Check if the checksum of the file is correct.
+    - Extract the archive to the `build` directory in the install path if it not already extracted.
+    """
+    if not install_path.exists():
+        install_path.mkdir()
+
+    entry = _get_entry()
+    uri = urlparse(entry["uri"])
+    logger.debug(f"downloading mAPI executable from: {uri.path}")
+    filename = os.path.basename(uri.path)
+    os.makedirs(install_path / HEADER_SV_VERSION, exist_ok=True)
+    download_path = install_path / HEADER_SV_VERSION / filename
+
+    if not download_path.exists():
+        # Download the build.
+        r = requests.get(entry["uri"])
+        with open(download_path, "wb") as f:
+            f.write(r.content)
+
+    # Extract the build.
+    output_path = install_path / HEADER_SV_VERSION / entry['dirname']
+    if not output_path.exists():
+        with zipfile.ZipFile(download_path, 'r') as z:
+            z.extractall(install_path / HEADER_SV_VERSION)
+
+    if download_path.exists():
+        os.remove(download_path)
+
+
+def download_and_install_jre(install_path: pathlib.Path) -> None:
+    """
+    Identify the correct pre-built release for the running Python version and:
+    - Download it if it is not already downloaded.
+    - Check if the checksum of the file is correct.
+    - Extract the archive to the `build` directory in the install path if
+    it not already extracted.
+    """
+    if not install_path.exists():
+        install_path.mkdir()
+
+    entry = _get_entry()
+    uri = urlparse(entry["jre_uri"])
+    logger.debug(f"downloading java runtime from: {uri.path}")
+    filename = os.path.basename(uri.path)
+    os.makedirs(install_path, exist_ok=True)
+    download_path = install_path / filename
+
+    if not download_path.exists():
+        # Download the build.
+        r = requests.get(entry["jre_uri"])
+        with open(download_path, "wb") as f:
+            f.write(r.content)
+
+    # Extract the build.
+    output_path = install_path / "jre"
+    if not output_path.exists():
+        with zipfile.ZipFile(download_path, 'r') as z:
+            z.extractall(install_path)
+
+    if download_path.exists():
+        os.remove(download_path)
+
+
+def download_and_install(install_path: pathlib.Path) -> None:
+    download_and_install_jar(install_path)
+    download_and_install_jre(install_path)
+
+
+def get_run_command(install_path: pathlib.Path) -> str:
+    """
+    Work out where the executable is located for the given platform/architecture.
+    """
+    entry = _get_entry()
+    return f"{install_path / entry['jre_dirname'] / 'bin' / 'java.exe'} -jar " \
+           f"{install_path / HEADER_SV_VERSION / entry['exe']}"
+
+
+def load_env_vars() -> None:
+    env_vars = {
+        "PYTHONUNBUFFERED": "1"
+    }
+    os.environ.update(env_vars)
+    from dotenv import load_dotenv
+    env_path = pathlib.Path(MODULE_DIR) / 'exe-config' / '.env'
+    load_dotenv(dotenv_path=env_path)
+
+
+if __name__ == "__main__":
+    # This will download and extract the correct build to the local `zzz` directory for testing.
+    os.environ['SDK_PORTABLE_MODE'] = "1"
+    SDK_PORTABLE_MODE = int(os.environ['SDK_PORTABLE_MODE'])
+    download_path = pathlib.Path("zzz")
+    config = Config()
+    # download_and_install_jar(download_path)
+    download_and_install_jre(download_path)
+
+    run_path = get_run_command(download_path)
