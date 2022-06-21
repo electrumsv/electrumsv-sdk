e35ac0401ef4b424f8e6480a641dd8ea07b2c31f
AustEcon <AustEcon0922@gmail.com>
2022-06-21 18:08:03 +1200

    Bug fix: When running the SDK as a python library (for use in scripts), the FileHandler gets instantiated multiple times producing duplication of output

--- electrumsv_sdk/app_state.py
+++ electrumsv_sdk/app_state.py
@@ -3,13 +3,13 @@ import logging
 import os
 from pathlib import Path
 import sys
-from typing import List
+from typing import List, Optional
 
 from electrumsv_node import electrumsv_node
 
 from .argparsing import ArgParser
 from .config import Config
-from .constants import NameSpace
+from .constants import NameSpace, LOG_LEVEL
 from .controller import Controller
 
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -24,6 +24,8 @@ orm_logger.setLevel(logging.WARNING)
 class AppState:
     """Global application state"""
 
+    file_handler: Optional[logging.FileHandler] = None
+
     def __init__(self, arguments: List[str]):
         self.argparser = ArgParser()
         self.argparser.manual_argparsing(arguments)  # allows library to inject args (vs sys.argv)
@@ -32,8 +34,10 @@ class AppState:
         if self.cli_inputs.namespace == NameSpace.CONFIG:
             self.config.print_json()
 
-        self.add_file_logging_for_sdk_logs()
-
+        # When using the SDK as a python library, AppState can be instantiated multiple times
+        # which results in numerous instances of FileHandler being added to the root logger
+        if not AppState.file_handler:
+            self.add_file_logging_for_sdk_logs()
         self.argparser.validate_cli_args()
 
         self.calling_context_dir: Path = Path(os.getcwd())
@@ -47,12 +51,13 @@ class AppState:
         self.controller = Controller(self)
 
     def add_file_logging_for_sdk_logs(self):
-        root_logger = logging.getLogger('root')
+        root_logger = logging.getLogger()
         logfile_name = "sdk.log"
         log_dir = self.config.SDK_HOME_DIR / "logs" / "sdk"
         os.makedirs(log_dir, exist_ok=True)
-        file_handler = logging.FileHandler(log_dir / logfile_name)
-        root_logger.addHandler(file_handler)
+        AppState.file_handler = logging.FileHandler(log_dir / logfile_name)
+        AppState.file_handler.setLevel(LOG_LEVEL)
+        root_logger.addHandler(AppState.file_handler)
 
     def handle_first_ever_run(self) -> None:
         assert self.config.CONFIG_PATH is not None
