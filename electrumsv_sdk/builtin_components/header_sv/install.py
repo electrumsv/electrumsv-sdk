3304b5df54f00d69e3ba31f21a15a6716481af83
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-06 11:52:31 +0000

    Adding tarfile member sanitization to extractall()

--- electrumsv_sdk/builtin_components/header_sv/install.py
+++ electrumsv_sdk/builtin_components/header_sv/install.py
@@ -130,7 +130,26 @@ def download_and_install_jre(install_path: pathlib.Path) -> None:
     if not output_path.exists():
         if entry['jre_uri'].endswith('.tar.gz'):
             with tarfile.open(download_path, 'r') as tar:
-                tar.extractall(install_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(tar, install_path)
 
         elif entry['jre_uri'].endswith('.zip'):
             with zipfile.ZipFile(download_path, 'r') as zip:
